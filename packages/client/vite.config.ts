9a4b97c2d5a1ad6c554c6933fe9abc662524652e
ddony8128 <ddony0528@gmail.com>
2026-01-30 12:13:15 +0900

    init : use pnpm to make client and server

---
+++ packages/client/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config';
+import { fileURLToPath, URL } from 'node:url';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": "http://localhost:4000"
+    }
+  },
+});
